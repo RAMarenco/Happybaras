40784dfeb08125275640f40dd8943e0eb80475b0
RAMarenco <00080121@uca.edu.sv>
2024-05-10 00:32:00 -0600

    Add: Landing Page, Google Auth package

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import { GoogleOAuthProvider } from "@react-oauth/google";
+//import { CLIENTID } from "./consts";
+
 import "./reset.css"
 
 import Routes from './routes/routes';
@@ -9,6 +12,12 @@ const router = createBrowserRouter(Routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {
+      //<GoogleOAuthProvider clientId={CLIENTID}>
+    }
     <RouterProvider router={router}/>
+    {
+      //</GoogleOAuthProvider>
+    }
   </React.StrictMode>,
 )
