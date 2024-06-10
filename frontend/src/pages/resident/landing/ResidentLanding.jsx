fc2bdb7f1a2b420a8239f06b07f693f38c55d6ba
Michelle Rivas <00015121@uca.edu.sv>
2024-06-09 18:21:18 -0600

    👍 update: resident landing

--- frontend/src/pages/resident/landing/ResidentLanding.jsx
+++ frontend/src/pages/resident/landing/ResidentLanding.jsx
@@ -1,26 +1,42 @@
 import { useState } from "react";
-import QRContainer from "../../../components/qrContainer/qrContainer.jsx";
-import classes from "./ResidentLanding.module.scss"
-import InstructionsModal from "../../../components/modals/instructionsModal/instructionsModal.jsx";
+import { MdInfo } from "react-icons/md";
 import FilledButton from "../../../components/Buttons/Filled/FilledButton.jsx";
+import Bubble from "../../../components/bubble/bubble.jsx";
 import Instructions from "../../../components/instructions/instructions.jsx";
+import QRContainer from "../../../components/qrContainer/qrContainer.jsx";
+import classes from "./ResidentLanding.module.scss";
 
 const ResidentLanding = () => {
     const [showQR, setShowQR] = useState(false);
     const [showInstructions, setShowInstructions] = useState(false);
 
-    const handleGenerateQRClick = (F) => {
-        if (!showQR)
-            setShowQR(true)
+    const handleInfoClick = () => {
+        setShowInstructions(showInstructions ? false : true);
+        console.log("in");
     }
 
     return (
         <div className={classes["ResidentLandingContainer"]}>
-            {showInstructions
-                ?
-                    <Instructions />
-                :
-                <QRContainer onGoBack={() => { handleGoBack() }} />
+            {showInstructions &&
+                <div className={classes["modal-background"]}>
+                    <div className={classes["modal-container"]}>
+                        <div className={classes["instructions"]}>
+                            <Instructions />
+                        </div>
+                        <div className={classes["go-back"]}>
+                            <FilledButton text={"Go Back"} onClick={handleInfoClick} />
+                        </div>
+                    </div>
+                </div>
+            }
+            <div className={classes["qr-container"]}>
+                <QRContainer />
+            </div>
+
+            {!showInstructions &&
+                <div className={classes["icon-container"]}>
+                    <Bubble icon={<MdInfo />} color={"#6AA6C6"} handleClick={handleInfoClick} />
+                </div>
             }
         </div>
     )
